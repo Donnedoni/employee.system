7a845e1283d8ecb73ea1da36914ec9a1a35380de
Donnedoni Fillies <donnedonifillies@gmail.com>
2023-04-27 12:52:28 +0200

    fixed the @PutMapping which allows for updating the records

--- src/main/java/com/employee/employee/system/controller/EmployeeController.java
+++ src/main/java/com/employee/employee/system/controller/EmployeeController.java
@@ -4,14 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.employee.employee.system.exception.ResourceNotFoundException;
 import com.employee.employee.system.models.Employee;
 import com.employee.employee.system.repo.EmployeeRepo;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,19 +31,25 @@ public class EmployeeController {
         return employeeRepo.findAll();
     }
 
-    @GetMapping
-    public Optional<Employee>findOne(int empId){
-        return employeeRepo.findById(Employee.getEmpId());
-    }
-
     @PostMapping
     public Employee save(@Validated @NonNull @RequestBody Employee employee){
         return employeeRepo.save(employee);
     }
 
-    @PutMapping("{empId}") 
-    public Employee update(@Validated @NonNull @RequestBody Employee employee){
-        return employeeRepo.save(employee);
+    @PutMapping("{empId}")
+    public ResponseEntity<Employee> update(@PathVariable int empId,@RequestBody Employee employee) {
+        Employee update = employeeRepo.findById(empId)
+            .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + empId));
+
+        update.setFirstName(employee.getFirstName());
+        update.setLastName(employee.getLastName());
+        update.setEmail(employee.getEmail());
+        update.setDepartment(employee.getDepartment());
+        update.setContactNo(employee.getContactNo());
+
+        employeeRepo.save(update);
+
+        return ResponseEntity.ok(update);
     }
 
 }
---
+++ src/main/java/com/employee/employee/system/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.employee.employee.system.exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
